4b2366e6a380fca40d4107de0a55eab0de6552e3
AutumnTiger2878 <franmora2783@gmail.com>
2024-10-20 23:10:59 -0600

    Ultimos Cambio 10/20/24

--- enviarFormulario.js
+++ enviarFormulario.js
@@ -1,6 +1,19 @@
 function enviarFormulario() {
  
+    const nombre = document.getElementById("nombre").value;
     
-
+    
+    
+    const xmlData = `<usuario>
+    <nombre>${nombre}</nombre>
+   
+    </usuario>`;
+    
+    localStorage.setItem('usuarioXML', xmlData)
+    
+    
+    window.location.href = 'datos.html';
+    
+    return false;
 
 }
